b4e82f8ea9f9138181160ce165607fdcbcc66113
HasrilMuhammadIbal <113493778+HasrilMuhammadIbal@users.noreply.github.com>
2022-12-14 11:05:03 +0700

    Initial commit

---
+++ src/main/java/com/hasril/clenthasril/ClenthasrilApplication.java
@@ -0,0 +1,13 @@
+package com.hasril.clenthasril;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClenthasrilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClenthasrilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hasril/clenthasril/ClenthasrilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hasril.clenthasril;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClenthasrilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
